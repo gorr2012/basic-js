634b29101d9f4143b41b7d7897cc842f135bbfb1
gorr2012 <sergey_gornost@mail.ru>
2021-03-18 23:47:39 +0300

    refactor: make short solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,21 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  
-	if (members === null || members === undefined) return false;
-
-	let newMembers = [];
-
-	for(let i = 0; i < members.length; i++){
-		if(typeof members[i] === 'string') newMembers.push(members[i].trim().toUpperCase());
-	} 
-
-	newMembers.sort();
-	let str = '';
-
-	for(let i = 0; i < newMembers.length; i++){
-		str += newMembers[i][0];
-	}
-
-	return str;
+  return Array.isArray(members) ? members.filter(e => typeof e === 'string').map(e => e.trim().toUpperCase()[0]).sort().join('') : false;
 };
