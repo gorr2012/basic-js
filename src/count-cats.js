375c96780aff3796bf1acb667bfa24d899582968
gorr2012 <sergey_gornost@mail.ru>
2021-03-18 22:35:28 +0300

    refactor: make short solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,14 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  let counter = 0;
 
-	for(let i = 0; i < matrix.length; i++){
-		let idx = matrix[i].indexOf('^^');
-		while (idx != -1) {
-			counter++;
-			idx = matrix[i].indexOf('^^', idx + 1);
-		}
-	}
+  let counter = 0;
+  matrix.map(e => e.map(el => {if(el === '^^') counter++ }) );
 	return counter;
 };
