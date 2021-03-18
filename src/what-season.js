6d96916cb029295a1d1c12455e93dbc36d482224
gorr2012 <sergey_gornost@mail.ru>
2021-03-19 00:00:57 +0300

    refactor: make short solution

--- src/what-season.js
+++ src/what-season.js
@@ -3,11 +3,6 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function getSeason(date) {
   if(!date) return 'Unable to determine the time of year!';
   if(!date.getTime()) throw Error ('Err');
-
-  let seasonNumber = Math.floor((date.getMonth() + 1) / 3) % 4;
-
-  if(seasonNumber === 0) return 'winter';
-  if(seasonNumber === 1) return 'spring';
-  if(seasonNumber === 2) return 'summer';
-  if(seasonNumber === 3) return 'autumn';
+  let seasonsArr = ['winter', 'spring', 'summer', 'autumn'];
+  return seasonsArr[Math.floor((date.getMonth() + 1) / 3) % 4];
 };
